d84a498646f3069ff005ce46ab1507b57dd41ff5
abelbark <106646800+abelbark@users.noreply.github.com>
2022-07-09 19:33:27 -0700

    Update app.js

--- MockUp/Comp380Project/Survey Form/app.js
+++ MockUp/Comp380Project/Survey Form/app.js
@@ -1,25 +1,48 @@
-   let users = [];
+let users = [];
 
-    const addUsers = (ev)=>{
-        ev.preventDefault();
-        let user = {
-            weight: document.getElementById('userInput1').value,
-            height: document.getElementById('userInput2').value,
-            gender: document.querySelector('input[name="gender"]:checked').value,
-            age: document.getElementById('userInput4').value,
-            goal: document.querySelector('input[name="diet"]:checked').value,
-            activity: document.querySelector('input[name="exercise"]:checked').value,
-            lifestyle: document.querySelector('input[name="lifestyle"]:checked').value,
-        }
-        users.push(user);
-        document.forms[0].reset();
-        console.warn('added', {users} );
-       
-        localStorage.setItem('HealthInfo', JSON.stringify(users) );
-        var retrievedObject = localStorage.getItem('HealthInfo');
-        console.log('retrievedObject: ', JSON.parse(retrievedObject));
-
+const addUsers = (ev)=>{
+    ev.preventDefault();
+    let user = {
+        weight: document.getElementById('userInput1').value,
+        height: document.getElementById('userInput2').value,
+        gender: document.querySelector('input[name="gender"]:checked').value,
+        age: document.getElementById('userInput4').value,
+        goal: document.querySelector('input[name="diet"]:checked').value,
+        activity: document.querySelector('input[name="exercise"]:checked').value,
+        lifestyle: document.querySelector('input[name="lifestyle"]:checked').value,
     }
+    users.push(user);
+    document.forms[0].reset();
+    console.warn('added', {users} );
+
+    
+    if(user.lifestyle == 'Vegetarian'){
+        alert('you chose Vegetarian');
+        return false;
+    }
+    else if(user.lifestyle == 'Vegan'){
+        confirm('you chose vegan');
+        return false;
+    }
+    else if(user.lifestyle == 'Pescatarian'){
+        confirm('you chose Pescatarian');
+        return false;
+    }
+    else if(user.lifestyle == 'Not Following Diet'){
+        confirm('Check out our Variety of Meals');
+        return false;
+    }
+    else{
+        confirm('What?');
+    }
+
+
+    localStorage.setItem('HealthInfo', JSON.stringify(users) );
+    var retrievedObject = localStorage.getItem('HealthInfo');
+    console.log('retrievedObject: ', JSON.parse(retrievedObject));
+
+}
+ 
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('btn').addEventListener('click', addUsers)
